899a09871c6628bea10e116710fe6c0462d54baf
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 17:39:16 +1000

    add left pane data

---
+++ src/components/AnalyticsPanel/left-panel-data.js
@@ -0,0 +1,38 @@
+const items = [
+  {
+    name: 'GL',
+    addition: '⬆️',
+    value: '264,738',
+    color: '#FF9900'
+  },
+  {
+    name: 'DS',
+    addition: '👍',
+    value: '64,738',
+    color: '#127BF5'
+  },
+  {
+    name: 'DV',
+    addition: '⬆️',
+    value: '23,738',
+    color: '#F73859'
+  },
+  {
+    name: 'NB',
+    addition: '🔥',
+    value: '8,837',
+    color: '#3DDDAA'
+  },
+  {
+    name: 'LY',
+    addition: '⬆️',
+    value: '5,375',
+    color: '#127BF5'
+  },
+  {
+    name: 'DK',
+    addition: '👎',
+    value: '3,923',
+    color: '#84868A'
+  }
+]
