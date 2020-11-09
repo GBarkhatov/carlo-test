aa2095c1816bf581cd81aed251c51a87ec884d80
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 14:05:32 +1000

    update container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,10 +3,19 @@ import styled from 'styled-components/macro'
 
 const Component = styled.div`
   display: flex;
-  min-height: calc(100vh - 40px - 64px);
+  min-height: calc(100vh - 56px);
   justify-content: center;
   align-items: center;
   background-color: #E5E5E5;
+
+  @media screen and (min-width: 750px) {
+    min-height: calc(100vh - 64px);
+  }
+
+  @media screen and (min-width: 1280px) {
+    min-height: calc(100vh - 64px - 40px);
+    padding-top: 71px;
+  }
 `
 
 const Container = ({ children }) => (
