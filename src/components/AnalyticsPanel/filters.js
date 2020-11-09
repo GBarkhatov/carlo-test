e47add28264bdffa4302a938ca2ed343dc8a533e
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 18:38:44 +1000

    update data

--- src/components/AnalyticsPanel/filters.js
+++ src/components/AnalyticsPanel/filters.js
@@ -1,4 +1,4 @@
-const items = [
+const active = [
   {
     name: 'Glyphy',
     value: '6,648',
@@ -31,18 +31,84 @@ const items = [
   }
 ]
 
+const visited = [
+  {
+    name: 'Drawkit',
+    value: '56,156',
+    color: '#FF9900',
+    short: 'DK'
+  },
+  {
+    name: 'Google',
+    value: '48,729',
+    color: '#127BF5',
+    short: 'GL'
+  },
+  {
+    name: 'Reddit',
+    value: '35,728',
+    color: '#F73859',
+    short: 'RD'
+  },
+  {
+    name: 'Upwork',
+    value: '21,829',
+    color: '#3DDDAA',
+    short: 'UP'
+  },
+  {
+    name: 'Facebook',
+    value: '12,281',
+    color: '#127BF5',
+    short: 'FB'
+  }
+]
+
+const events = [
+  {
+    name: 'Today',
+    value: '8,125',
+    color: '#FF9900',
+    short: '5/10'
+  },
+  {
+    name: 'Yesterday',
+    value: '2,258',
+    color: '#127BF5',
+    short: '4/10'
+  },
+  {
+    name: 'Last week',
+    value: '13,421',
+    color: '#F73859',
+    short: '1-7'
+  },
+  {
+    name: 'Last Month',
+    value: '15,927',
+    color: '#3DDDAA',
+    short: 'SEP'
+  },
+  {
+    name: 'Last Year',
+    value: '25,125',
+    color: '#127BF5',
+    short: '2019'
+  }
+]
+
 const filters = [
   {
     title: 'Most Active Sites',
-    data: items
+    data: active
   },
   {
     title: 'Most Visited Sites',
-    data: items
+    data: visited
   },
   {
     title: 'Top Events',
-    data: items
+    data: events
   }
 ]
 
