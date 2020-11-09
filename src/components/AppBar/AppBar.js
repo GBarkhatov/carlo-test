427ba64279a7aeb4bdeac0312daad0d9490d66ed
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 12:57:24 +1000

    change sections

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -33,11 +33,20 @@ const useStyles = makeStyles((theme) => ({
       flexGrow: 1
     }
   },
+  search: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'block'
+    }
+  },
   logo: {
     flexGrow: 1,
     [theme.breakpoints.up('md')]: {
       flexGrow: 0
     }
+  },
+  accountSection: {
+    display: 'flex'
   },
   appBar: {
     backgroundColor: '#101010'
@@ -45,18 +54,6 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     display: 'block',
     marginRight: theme.spacing(2),
-    [theme.breakpoints.up('md')]: {
-      display: 'none'
-    }
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex'
-    }
-  },
-  sectionMobile: {
-    display: 'flex',
     [theme.breakpoints.up('md')]: {
       display: 'none'
     }
@@ -83,7 +80,6 @@ const AppBarComponent = () => {
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null)
 
   const isMenuOpen = Boolean(anchorEl)
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl)
 
   const [open, setOpen] = React.useState(false)
 
@@ -123,63 +119,6 @@ const AppBarComponent = () => {
     setMobileMoreAnchorEl(event.currentTarget)
   }
 
-  const menuId = 'primary-search-account-menu'
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  )
-
-  const mobileMenuId = 'primary-search-account-menu-mobile'
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label='show 4 new mails' color='inherit'>
-          <Badge badgeContent={4} color='secondary'>
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label='show 11 new notifications' color='inherit'>
-          <Badge badgeContent={11} color='secondary'>
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label='account of current user'
-          aria-controls='primary-search-account-menu'
-          aria-haspopup='true'
-          color='inherit'
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  )
-
   return (
     <div className={classes.grow}>
       <AppBar position='static' className={classes.appBar}>
@@ -197,8 +136,12 @@ const AppBarComponent = () => {
             <Logo src={logo} alt='BetterMeter' />
           </StyledLink>
           <div className={classes.growDivider} />
-          <div className={classes.sectionDesktop}>
-            <IconButton aria-label='show 9 new notifications' color='inherit'>
+          <div className={classes.accountSection}>
+            <IconButton
+              aria-label='show 9 new notifications'
+              color='inherit'
+              className={classes.search}
+            >
               <SearchIcon />
             </IconButton>
             <IconButton aria-label='show 9 new notifications' color='inherit'>
@@ -209,23 +152,12 @@ const AppBarComponent = () => {
             <IconButton
               edge='end'
               aria-label='account of current user'
-              aria-controls={menuId}
+              aria-controls='primary-search-account-menu'
               aria-haspopup='true'
               onClick={handleProfileMenuOpen}
               color='inherit'
             >
               <AccountCircle />
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label='show more'
-              aria-controls={mobileMenuId}
-              aria-haspopup='true'
-              onClick={handleMobileMenuOpen}
-              color='inherit'
-            >
-              <MoreIcon />
             </IconButton>
           </div>
         </Toolbar>
@@ -258,8 +190,18 @@ const AppBarComponent = () => {
           ))}
         </List>
       </Drawer>
-      {renderMobileMenu}
-      {renderMenu}
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id='primary-search-account-menu'
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+      >
+        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      </Menu>
     </div>
   )
 }
