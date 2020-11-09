398f01434ab45f4d05c4e7af7a0d71c3083d65e9
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 11:23:06 +1000

    add components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 
+import AppBar from './components/AppBar/AppBar'
+import Analytics from './components/Analytics/Analytics'
+import FilterPanel from './components/FilterPanel/FilterPanel'
+
 const Component = styled.div``
 
 const App = () => (
   <Component>
-    {/* AppBar */}
-    {/* Analytics */}
-    {/* FilterPanel - MostActiveSites */}
-    {/* FilterPanel - MostVisitedSites */}
-    {/* FilterPanel - TopEvents */}
+    <AppBar />
+    <Analytics />
+    <FilterPanel />
+    <FilterPanel />
+    <FilterPanel />
   </Component>
 )
 
---
+++ src/components/FilterPanel/FilterPanel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+const Component = styled.div``
+
+const FilterPanel = () => (
+  <Component>
+    Inside FilterPanel
+  </Component>
+)
+
+export default FilterPanel
