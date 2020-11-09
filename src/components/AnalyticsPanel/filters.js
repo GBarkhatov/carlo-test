84b2eea9a521bb89f0d0d1b12799344b79c489fa
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 15:04:48 +1000

    add filters

---
+++ src/components/AnalyticsPanel/filters.js
@@ -0,0 +1,49 @@
+const items = [
+  {
+    name: 'Glyphy',
+    value: '6,648',
+    color: '#FF9900',
+    short: 'GL'
+  },
+  {
+    name: 'DesignStripe',
+    value: '1,374',
+    color: '#127BF5',
+    short: 'DS'
+  },
+  {
+    name: 'DesignValley',
+    value: '20,748',
+    color: '#F73859',
+    short: 'DV'
+  },
+  {
+    name: 'Neueubel',
+    value: '32,884',
+    color: '#3DDDAA',
+    short: 'NB'
+  },
+  {
+    name: 'Last Year',
+    value: '182,874',
+    color: '#127BF5',
+    short: 'LY'
+  }
+]
+
+const filters = [
+  {
+    title: 'Most Active Sites',
+    data: items
+  },
+  {
+    title: 'Most Visited Sites',
+    data: items
+  },
+  {
+    title: 'Top Events',
+    data: items
+  }
+]
+
+export default filters
