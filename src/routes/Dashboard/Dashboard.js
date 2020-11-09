dd7bca73b87c80862a9fc75f5e3ab4777f44ff23
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 11:43:36 +1000

    add appbar

--- src/routes/Dashboard/Dashboard.js
+++ src/routes/Dashboard/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 
+import AppBar from '../../components/AppBar/AppBar'
+
 const Component = styled.div``
 
 const Dashboard = () => (
   <Component>
-    Inside Dashboard
+    <AppBar />
+    <p>Dashboard content</p>
   </Component>
 )
 
