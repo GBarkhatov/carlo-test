3bf386bbd419d81767c6a6a691bd484bf5fe8cc6
Govard Barkhatov <gbarkhatov@gmail.com>
2020-11-09 13:47:58 +1000

    add border right

--- src/components/AppBar/Links.js
+++ src/components/AppBar/Links.js
@@ -23,6 +23,7 @@ const Button = styled(Link)`
     props.match === 'yes' ? '#F8F5F5' : '#FFFFFF'};
   font-weight: ${(props) => (props.match === 'yes' ? 700 : 400)};
   font-size: 0.9rem;
+  border-right: 1px solid #E5E5E5;
 
   &:hover {
     filter: brightness(110%);
